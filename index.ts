fded6d366bb4fd8a60ea2f7613d211913f0cee4f
Lorenzo Elenkov <lorenzoelenkov@coherentsolutions.com>
2023-08-14 14:19:23 +0300

    init

--- index.ts
+++ index.ts
@@ -16,9 +16,13 @@ io.on('connection', async socket => {
   });
 });
 
-// emit a message to the client, who has previously joined a room with a name of the app id
-io
-  .to('appId') // change 'appId' here with the actual application id as string
-  .emit('typeOfMessage', "Message content"); // change 'typeOfMessage' for example with 'success', 'warning', 'error', 'info'; second argument is the actual content to be streamed to the client
+// example function
+const getMessage = () => {
+  // something happens, which after we have to send the stream message to the client
+  // ...
+  // emit a message to the client, who has previously joined a room with a name of the app id
+  io.to('appId') // change 'appId' here with the actual application id as string
+    .emit('typeOfMessage', 'Message content'); // change 'typeOfMessage' for example with 'success', 'warning', 'error', 'info'; second argument is the actual content to be streamed to the client
+};
 
 server.listen(port, () => console.log(`Connected to ${port}`));
