26af94f7682a5b99bcf1c888ee1ee62ad6359639
Ioana Grigore <ioanagrigore@coherentsolutions.com>
2023-08-17 10:41:26 +0300

    sending initialize test message

--- index.ts
+++ index.ts
@@ -22,32 +22,19 @@ io.on('connection', (socket) => {
     console.log(`User joined room ${roomId}`);
 
     setInterval(() => {
-      const message = `Message from server at ${new Date().toLocaleTimeString()} in room ${roomId}`;
+      const message = {
+        type: "initialize",
+        severity: "info",
+        message: "Initialize app works"
+      }
+      console.log(message);
       io.to(roomId).emit('message', message); // Emit to specific room
-    }, 4000);
+    }, 10000);
   });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
-
-// // example function
-// const getMessage = (appId: string) => {
-//   // something happens, which after we have to send the stream message to the client
-//   // ...
-//   console.log("get message")
-//   // emit a message to the client(s), who have previously joined a room with a name of the app id
-//   io.to(appId) // change 'appId' here with the actual application id as string
-//     .emit("newStatus", {
-//       severity: "SEVERITY", // replace 'SEVERITY' with 'info', 'warning', 'error', 'success'
-//       message: "Example", // replace 'Example' with status message
-//     });
-// };
-
-// app.post("/trigger-message/:appId", (req, res) => {
-//   const { appId } = req.params;
-//   getMessage(appId);
-// });
 
 server.listen(port, () => console.log(`Connected to ${port}`));
